982a1b56cfa595e1bacf46aa509fa5ffba3fcae5
Raneet1703 <134468606+Raneet1703@users.noreply.github.com>
2024-09-01 13:57:54 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,15 @@
-resource "aws_s3_bucket_versioning" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning_configuration {
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket = "example-bucket"
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+  versioning_configuration {
     status = "Enabled"
   }
-  
 }
